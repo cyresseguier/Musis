787bf1b15d28e571eae13fc2abda641c6aca1f27
Bertrand Junqua <bertrandjun@gmail.com>
2014-12-15 14:50:42 +0100

    Ajout Conf Compass

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "symfony/web/assets/css"
+sass_dir = "symfony/src/Musis/MainBundle/Resources/public/scss"
+images_dir = "symfony/web/assets/img"
+javascripts_dir = "symfony/web/assets/js"
+
+output_style = :expanded # :expanded or :nested or :compact or :compressed
+
+relative_assets = true
+
+# line_comments = false
