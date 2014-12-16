ffc88353fa96e4efa20d24e73282d9d34d812f47
Bertrand Junqua <bertrandjun@gmail.com>
2014-12-16 15:22:06 +0100

    Suppression Bower

--- src/Team/MusisBundle/Resources/public/js/concat/output.js
+++ src/Team/MusisBundle/Resources/public/js/concat/output.js
@@ -1,1 +1,11 @@
-var blop = "";
+var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+});
+
+var map = L.map('musisMap', {
+    scrollWheelZoom: false,
+    center: [40.7127837, -74.0059413],
+    zoom: 6
+});
+
+map.addLayer(layer);
