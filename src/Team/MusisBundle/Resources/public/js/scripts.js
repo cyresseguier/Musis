d047eac2ac8f4a23580eb27d5adc3afb8ca0b087
U-pc-CYriuk\CYriuk <CYriuk@pc-CYriuk.(none)>
2015-02-28 18:12:44 +0100

    Premier prototype player Carte fonctionnel

--- src/Team/MusisBundle/Resources/public/js/scripts.js
+++ src/Team/MusisBundle/Resources/public/js/scripts.js
@@ -49,57 +49,50 @@ $(document).ready(function() {
 
 	var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png');
 
-	var map = L.map('map');
+	var map = L.map('map').setView([48.85, 2.33], 13);
 
+	var wayLayer;
 
-	var container=L.Routing.control({
-		plan: L.Routing.plan(
-			[
-				L.latLng(48.85837009999999, 2.2944813000000295),
-				L.latLng(48.851264, 2.3760990000000675),
-				L.latLng(48.85706949999999, 2.359624599999961),
-				L.latLng(48.85837009999999, 2.2944813000000295)
-			],
-			{
-        		createMarker: function(i, wp) {
-            		return L.marker(wp.latLng, {
-                	draggable: false
-            		}).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-        		}
-    		}),
-		addWaypoints: false,
-		routeWhileDragging: false
-	}).addTo(map);
-	L.Routing.control({
-		plan: L.Routing.plan(
-			[
-				L.latLng(48.89321700000001, 2.287864000000013),
-				L.latLng(48.86244, 2.2491730000000416),
-				L.latLng(48.89321700000001, 2.287864000000013),
-			],
-			{
-				createMarker: function(i, wp) {
-					return L.marker(wp.latLng, {
-						draggable: false,
-					});
-				},
-			}
-		),
-		routeWhileDragging: false
-	}).addTo(map);
+	function createRouting(PlaylistRoute){	
+		if (wayLayer!=null) map.removeLayer(wayLayer);
+		
+		wayLayer=L.Routing.control({
+			plan: L.Routing.plan(
+				PlaylistRoute,
+				{
+	        		createMarker: function(i, wp) {
+	            		var marker = new L.marker(wp.latLng, {
+	                		draggable: false
+	            		});
+	            		marker.on('click', onClick);
+	        			return marker;
+	        		}
+	    		}),
+			addWaypoints: false,
+			routeWhileDragging: false
+		});
 
-
-	//var marker = L.marker([48.8642701, 2.3534680999999864]).addTo(map);
+		map.addLayer(wayLayer);
+	}
 
 	map.addLayer(layer);
-	
-	//NOT WORKING
-	/*$(".leaflet-marker-icon").click(function () {
-		alert("test");
-		panelManage("open", "#playlist");
-		DZ.player.playAlbum(302127);
-	});*/
 
+	function onClick(e) {
+		//Get Latitude and Longitude of the element clicked
+		var Place = this.getLatLng();
+		playTrackByPlace(Place);
+
+	}
+
+	function setPlaylistRoute(Playlist){
+		var PlaylistRoute=[];
+		for(var i=0;i<Playlist.length;i++){
+			PlaylistRoute.push(L.latLng(Playlist[i].places[0].coordLat,Playlist[i].places[0].coordLong));
+		}
+		// We close the route // WILL IMPROVE
+		PlaylistRoute.push(L.latLng(Playlist[0].places[0].coordLat,Playlist[0].places[0].coordLong));
+		return PlaylistRoute;
+	}
 	// INTRO SCROLL
 
 	$("#begin").click(function () {
@@ -130,11 +123,20 @@ $(document).ready(function() {
 	function event_listener_append() {
 		var pre = document.getElementById('event_listener');
 		var line = [];
+		var min,sec;
+
 		for (var i = 0; i < arguments.length; i++) {
 			line.push(arguments[i]);
 		}
 		pre.innerHTML += line.join(' ') + "\n";
-		$("#song-info .position").text(Math.floor(arguments[1]/60)+":"+(Math.floor(arguments[1])%60));
+		min=Math.floor(arguments[1]/60);
+		sec=Math.floor(arguments[1])%60;
+
+		if(sec<10) sec="0"+sec;
+
+		// Show position if only you got numeric value (time song) 
+		if (!isNaN(min)||!isNaN(sec))
+			$("#song-info .position").text(min+":"+sec);
 	}
 
 	function onPlayerLoaded() {
@@ -142,6 +144,7 @@ $(document).ready(function() {
 		event_listener_append('player_loaded');
 		DZ.Event.subscribe('current_track', function(arg){
 			event_listener_append('current_track', arg.index, arg.track.title, arg.track.album.title);
+
 			$("#song-info .title").text(arg.track.artist.name+" - "+arg.track.title);
 			$("#song-info .length").text(Math.floor(arg.track.duration/60)+":"+(arg.track.duration%60));
 		});
@@ -162,21 +165,67 @@ $(document).ready(function() {
 			onload : onPlayerLoaded
 		}
 	});
-});
 
-//MUSIS ENGINE MUSIC PART
+	function playPlaylist(Playlist){
+		var playlistLink=[];
 
-function loadPlaylist(tab,playlistName){
+		for (var i=0; i<Playlist.length; i++){
+			playlistLink.push(Playlist[i].link);
+		}
 
-	var playlist=[];
-	for (var i=0; i<tab.length; i++){
-		for (var j=0; j<tab[i].playlists.length; j++){
-			if (tab[i].playlists[j].name==playlistName){
-				playlist.push(tab[i].link);
+		DZ.player.playTracks(playlistLink); 
+	}	
+	
+	function playTrackByPlace(Place){
+		for (var i=0; i<globalPlaylist.length; i++){
+
+			//console.log(Place.lng);
+			//console.log(globalPlaylist[i].places[0].coordLong);
+			if(globalPlaylist[i].places[0].coordLong==Place.lng){
+				switchTrack(i,DZ.player.getCurrentIndex());
 			}
 		}
 	}
-	DZ.player.playTracks(playlist); 
-	
-	return false;
-}
+
+	// TO IMPROVE
+	function switchTrack(id,position){
+		console.log(id,position);
+
+		if (id>position){
+			for(var i=position;i<id;i++){
+				DZ.player.next();
+			}
+		}
+		else if (id<position){
+			for(var j=id;j<position;j++){
+				DZ.player.prev();
+			}
+		}
+
+		else{}
+	}
+
+	//MUSIS ENGINE MUSIC PART
+	var globalPlaylist=[];
+
+	window.loadPlaylist = function(tab,playlistName){
+		var playlist=[];
+		for (var i=0; i<tab.length; i++){
+			for (var j=0; j<tab[i].playlists.length; j++){
+				if (tab[i].playlists[j].name==playlistName){
+					playlist.push(tab[i]);
+				}
+			}
+		}
+
+		playPlaylist(playlist);
+
+		PlaylistRoute=setPlaylistRoute(playlist);
+		createRouting(PlaylistRoute);
+		
+		globalPlaylist=playlist;
+		return playlist;
+	};
+
+});
+
